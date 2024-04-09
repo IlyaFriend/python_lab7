292bc8230ad212513a496145bf73deea2fb92230
IlyaFriend <eliasbell2233@gmail.com>
2024-04-09 19:50:21 +0300

    fix main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-from input import input_text_from_console, read_from_file_with_builtin, read_from_file_with_pandas
-from output import output_text_to_console, write_to_file_with_builtin
+from app.io.input import input_text_from_console, read_from_file_with_builtin, read_from_file_with_pandas
+from app.io.output import output_text_to_console, write_to_file_with_builtin
 
 def main():
-    # Ввод тексту з консолі
-    text_from_console = input_text_from_console()
-    print("Text from console:", text_from_console)
-    output_text_to_console()
-    write_to_file_with_builtin(text_from_console)
+    # Call the function to input text from the console
+    console_text = input_text_from_console()
 
-    # Зчитування з файлу за допомогою вбудованих можливостей Python
-    text_from_builtin = read_from_file_with_builtin()
-    print("Text from file with built-in:", text_from_builtin)
-    output_text_to_console()
-    write_to_file_with_builtin(text_from_builtin)
+    # Print the console text to the console both with print and output_text_to_console
+    print("print:", console_text)
+    output_text_to_console(console_text)
+    print('_________________________________________\n')
 
-    # Зчитування з файлу за допомогою бібліотеки pandas
-    data_from_pandas = read_from_file_with_pandas()
-    print("Data from file with pandas:", data_from_pandas)
-    output_text_to_console()
-    write_to_file_with_builtin(data_from_pandas)
+    # Write the console text to a file
+    write_to_file_with_builtin("data/test.txt", console_text)
+
+    # Call the function to read from a file using Python's built-in capabilities
+    file_content_builtin = read_from_file_with_builtin("data/test.txt")
+
+    # Print the file content (read using built-in capabilities) to the console
+    print("File content (built-in):", file_content_builtin)
+    output_text_to_console(file_content_builtin)
+    print('_________________________________________\n')
+    
+    # Call the function to read from a file using pandas
+    file_data_pandas = read_from_file_with_pandas("data/file_data_pandas_input.csv")
+
+    # Print the file data (read using pandas) to the console
+    print("File data (pandas):\n", file_data_pandas)
+
+    # Write the file data (read using pandas) to a file (as CSV)
+    file_data_pandas.to_csv("data/file_data_pandas_output.csv", index=False)
 
 if __name__ == "__main__":
     main()
