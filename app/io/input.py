ef7ad51f7d887a442c12980624bbc002d67edd03
IlyaFriend <eliasbell2233@gmail.com>
2024-04-09 18:38:14 +0300

    add docstrings for the io package's functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,8 +1,26 @@
 def input_text_from_console():
+    """
+    Function for inputting text from the console.
+
+    Returns:
+        str: The text entered by the user.
+    """
     pass
 
 def read_from_file_with_builtin():
+    """
+    Function for reading from a file using Python's built-in capabilities.
+
+    Returns:
+        str: The content of the file.
+    """
     pass
 
 def read_from_file_with_pandas():
+    """
+    Function for reading from a file using the pandas library.
+
+    Returns:
+        DataFrame: A DataFrame object containing the data from the file.
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,5 +1,11 @@
 def output_text_to_console():
+    """
+    Function for outputting text to the console.
+    """
     pass
 
 def write_to_file_with_builtin():
+    """
+    Function for writing to a file using Python's built-in capabilities.
+    """
     pass
