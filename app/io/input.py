e99b59ad1b5f8aed3e1318ef77873071c51df748
IlyaFriend <eliasbell2233@gmail.com>
2024-04-09 18:36:34 +0300

    add blank functions in the io package

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def input_text_from_console():
+    pass
+
+def read_from_file_with_builtin():
+    pass
+
+def read_from_file_with_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def output_text_to_console():
+    pass
+
+def write_to_file_with_builtin():
+    pass
