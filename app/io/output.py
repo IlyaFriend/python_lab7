338a5565f62be0dc88216980f70b5ee717a7c8b8
IlyaFriend <eliasbell2233@gmail.com>
2024-04-09 19:50:08 +0300

    write io functions

--- app/io/output.py
+++ app/io/output.py
@@ -1,11 +1,22 @@
-def output_text_to_console():
+def output_text_to_console(text):
     """
     Function for outputting text to the console.
-    """
-    pass
 
-def write_to_file_with_builtin():
+    Args:
+        text (str): The text to be outputted.
+    """
+    print("Output text to console:", text)
+
+def write_to_file_with_builtin(filename, text):
     """
     Function for writing to a file using Python's built-in capabilities.
+
+    Args:
+        text (str): The text to be written to the file.
     """
-    pass
+    try:
+        with open(filename, 'w') as file:
+            file.write(text)
+        print("Data written to file successfully!")
+    except FileNotFoundError:
+        print("File not found!")
